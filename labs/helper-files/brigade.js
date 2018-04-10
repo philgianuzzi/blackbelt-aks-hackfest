d72b01970fc24e4d7f35dda855731fb5a8ac57e1
philgianuzzi <phil.gianuzzi@gmail.com>
2018-04-10 11:23:00 -0400

    Update brigade.js

--- labs/helper-files/brigade.js
+++ labs/helper-files/brigade.js
@@ -58,7 +58,7 @@ function dockerJobRunner(config, d) {
 
 function kubeJobRunner (config, k) {
     k.storage.enabled = false
-    k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
+    k.image = "azureworkshop/rating-web:v1"
     k.tasks = [
         `kubectl set image deployment/heroes-web-deploy heroes-web-cntnr=<youracrhere>.azurecr.io/azureworkshop/rating-web:${config.get("imageTag")}`
     ]
