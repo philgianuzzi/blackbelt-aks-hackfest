35447a014014284dccfea85157839c6b650ac679
philgianuzzi <phil.gianuzzi@gmail.com>
2018-04-10 14:22:35 -0400

    Update Footer.vue
    jkdjdjdjd

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Phil Gqwqwqw
+        Created by Phil
       </div>
       <div class="col-lg-6">
       </div>
